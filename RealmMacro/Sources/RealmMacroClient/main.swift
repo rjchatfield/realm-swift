05c6a21f78c814601faf4e3e6ed1f39727e803c9
Robert J Chatfield <rchatfield@atlassian.com>
2024-02-04 21:50:06 +1000

    rename & tidy up

--- RealmMacro/Sources/RealmMacroClient/main.swift
+++ RealmMacro/Sources/RealmMacroClient/main.swift
@@ -5,27 +5,27 @@ import Foundation
 /*
  Example object that ensures all generated code is valid
  */
-@RealmSchemaDiscovery
-final class FooObject: Object {
+@CompileTimeSchema
+open class FooObject: Object {
     @Persisted(primaryKey: true) var id: String
     @Persisted var name: String
-    @Persisted(indexed: true) var key: String
-    @Persisted var nestedObject: NestedObject?
-    @Persisted var embeddedObjects: List<NestedObject>
+    @Persisted(indexed: true) internal var key: String
+    @Persisted public internal(set) var nestedObject: NestedObject?
+    @Persisted private var embeddedObjects: List<NestedObject>
 
     var computed: String { "" }
     func method() {}
 
-    @RealmSchemaDiscovery
+    @CompileTimeSchema
     @objc(ObjcNestedObject)
-    class NestedObject: Object {
+    public class NestedObject: Object {
         @Persisted(primaryKey: true) var id: String
         @Persisted var name2: String
     }
 
-    @RealmSchemaDiscovery
+    @CompileTimeSchema
     @objc(ObjcNestedEmbeddedObject)
-    class NestedEmbeddedObject: EmbeddedObject {
+    private final class NestedEmbeddedObject: EmbeddedObject {
         @Persisted var name3: String
     }
 }
